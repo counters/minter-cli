5e37a4d68d7bce9dd23aaf9ad88520e7877dd9aa
webcounters <webcounters@gmail.com>
2025-04-10 23:13:58 +0300

    fix address

--- src/commands/wallet/WalletGenerateCommand.ts
+++ src/commands/wallet/WalletGenerateCommand.ts
@@ -9,7 +9,7 @@ export class WalletGenerateCommand extends CommandRunner {
 
     async run(passedParams: string[], options?: Record<string, any>): Promise<void> {
         const wallet : Wallet = generateWallet();
-        console.log(wallet.getPublicKeyString());
+        console.log(wallet.getAddressString());
         console.log(wallet.getMnemonic());
         // process.stdout.write(`minter-cli version ${version}`);
         return Promise.resolve(undefined);
